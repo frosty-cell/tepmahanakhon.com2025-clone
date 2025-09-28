4ce3939febd8806b9d1631169b43cfab0b1d18c6
frosty-cell <65022353@up.ac.th>
2025-09-28 08:49:14 +0700

    SEO: meta-description add

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,7 @@
 
 <svelte:head>
     <title>[Clone] Home | TEP BAR</title>
+    <meta name="description" content="Experience the essence of Thai culture through music, cocktails, and ambiance. TEP BAR(เทพบาร์) — Thailand's only Cultural Bar, established in 2015." />
 </svelte:head>
 
 <main>
